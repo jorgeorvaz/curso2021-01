a507c6d6121ecef47bac7f3b2611c38f6dcac043
Jorge <jorge.ordonez@factorlibre.com>
2021-04-13 16:14:03 +0000

    first menu

--- helpdesk_jorgeordonez/models/helpdesk_ticket.py
+++ helpdesk_jorgeordonez/models/helpdesk_ticket.py
@@ -2,7 +2,9 @@ from odoo import fields, models
 
 
 class HelpdeskTicket(models.Model):
+    # nombre de la tabla en bbdd
     _name = 'helpdesk.ticket'
+    _description = 'Ticket'
 
     # string es para el nombre de la vista en odoo. el nombre de la variable es para bbdd
     name = fields.Char(string='Name')
