e9789e0776aebe58d2ba70188def47b4733d50f3
Jorge <jorge.ordonez@factorlibre.com>
2021-04-15 15:13:39 +0000

    comments deleted

--- helpdesk_jorgeordonez/models/helpdesk_ticket.py
+++ helpdesk_jorgeordonez/models/helpdesk_ticket.py
@@ -2,17 +2,13 @@ from odoo import fields, models
 
 
 class HelpdeskTicket(models.Model):
-    # nombre de la tabla en bbdd
     _name = 'helpdesk.ticket'
     _description = 'Ticket'
 
-    # string es para el nombre de la vista en odoo. el nombre de la variable es para bbdd
     name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
     date = fields.Date(string='Date')
 
-    # - Añadir los siguiente campos:
-    # - Estado [Nuevo, Asignado, En proceso, Pendiente, Resuelto, Cancelado], que por defecto sea Nuevo
     state = fields.Selection([
         ('nuevo', 'Nuevo'),
         ('asignado', 'Asignado'),
@@ -21,19 +17,11 @@ class HelpdeskTicket(models.Model):
         ('resuelto', 'Resuelto'),
         ('cancelado', 'Cancelado')
     ], string='State', default='nuevo')
-    # - Tiempo dedicado (en horas)
+
     time = fields.Float(string='Time')
-    # - Asignado (tipo check)
     assigned = fields.Boolean(string='Assigned', readonly=True)
-    # - Fecha límite
     date_limit = fields.Date(string='Date Limit')
-    # - Acción correctiva (html)
     action_corrective = fields.Html(
         string='Corrective Action', help='Describe corrective actions to do')
-    # - Acción preventiva (html)
     action_preventive = fields.Html(
         string='Preventive Action', help='Describe preventive actions to do')
-    # El campo nombre que sea obligatorio
-    # En algún campo añadir un texto de ayuda indicando su funcionalidad, luego revisar que funciona.
-    # El campo Asignado:
-    # - hacer que sea solo de lectura
