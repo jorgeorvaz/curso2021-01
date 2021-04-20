f7aea7601a97349abc72a4299d16b817f42ab279
Jorge <jorge.ordonez@factorlibre.com>
2021-04-20 16:41:24 +0000

    orm methods

--- helpdesk_jorgeordonez/models/helpdesk_ticket.py
+++ helpdesk_jorgeordonez/models/helpdesk_ticket.py
@@ -5,9 +5,27 @@ class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
     _description = 'Ticket'
 
+    action_ids = fields.One2many(
+        comodel_name='helpdesk.ticket.action',
+        inverse_name='ticket_id',
+        string='Actions'
+    )
+
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket.tag',
+        relation='helpdesk_ticket_tag_rel',
+        column1='ticket_id',
+        column2='tag_id',
+        string='Tags'
+    )
+
     name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description', translate=True)
     date = fields.Date(string='Date')
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Assigned to'
+    )
 
     state = fields.Selection([
         ('nuevo', 'Nuevo'),
@@ -19,12 +37,53 @@ class HelpdeskTicket(models.Model):
     ], string='State', default='nuevo')
 
     time = fields.Float(string='Time')
-    assigned = fields.Boolean(string='Assigned', readonly=True)
+    assigned = fields.Boolean(
+        string='Assigned', readonly=True, compute='_compute_assigned')
     date_limit = fields.Date(string='Date Limit')
     action_corrective = fields.Html(
         string='Corrective Action', help='Describe corrective actions to do')
     action_preventive = fields.Html(
         string='Preventive Action', help='Describe preventive actions to do')
+
+    ticket_qty = fields.Integer(
+        string='Ticket Qty', compute='_compute_ticket_qty')
+
+    tag_name = fields.Char(
+        string='Tag name',
+    )
+
+    def create_tag(self):
+        self.ensure_one()
+
+        # Opción 1
+        self.write({
+            'tag_ids': [(0, 0, {'name': self.tag_name})]
+        })
+
+        # # Opción 2
+        # tag = self.env['helpdesk.ticket.tag'].create({'name': self.tag_name})
+        # self.write({
+        #     'tag_ids': [(4, tag.id, 0)]
+        # })
+        # # Opción 3
+        # tag = self.env['helpdesk.ticket.tag'].create({'name': self.tag_name})
+        # self.write({
+        #     'tag_ids': [(6, 0, tag.ids)]
+        # })
+
+        # # Opción 4
+        # tag = self.env['helpdesk.ticket.tag'].create({'name': self.tag_name})
+        # self.write({
+        #     'tag_ids': [(6, 0, self.ids)]
+        # })
+
+    @api.depends('user_id')
+    def _compute_ticket_qty(self):
+        # for record in self:
+        #     # search_count??????
+        #     other_tickets = self.env['helpdesk.ticket'].search([('user_id', '=', record.user_id.id)])
+        #     record.ticket_qty = len(other_tickets)
+        self.ticket_qty = self.env['helpdesk.ticket'].search_count([('user_id', '=', self.user_id.id)])
 
     def do_assign(self):
         self.ensure_one()
@@ -56,3 +115,39 @@ class HelpdeskTicket(models.Model):
     #     active_tickets = self.search[('active', '=', True)]
     #     for ticket in active_tickets:
     #         ticket.close()
+
+    @api.depends('user_id')
+    def _compute_assigned(self):
+        for record in self:
+            record.assigned = record.user_id and True or False
+
+
+class HelpdeskTicketAction(models.Model):
+    _name = 'helpdesk.ticket.action'
+    _description = 'Action'
+
+    name = fields.Char()
+    date = fields.Date()
+    ticket_id = fields.Many2one(
+        comodel_name='helpdesk.ticket',
+        string='Ticket'
+    )
+
+
+class HelpdeskTicketTag(models.Model):
+    _name = 'helpdesk.ticket.tag'
+    _description = 'Tag'
+
+    name = fields.Char()
+    date = fields.Date()
+    ticket_id = fields.Many2one(
+        comodel_name='helpdesk.ticket',
+        string='Ticket'
+    )
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket',
+        relation='helpdesk_ticket_tag_rel',
+        column1='tag_id',
+        column2='ticket_id',
+        string='Tags'
+    )
